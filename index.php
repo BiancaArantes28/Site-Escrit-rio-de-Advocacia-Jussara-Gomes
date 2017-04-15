571449509c95af8803ca4ed510556c52b87cce66
BiancaArantes28 <biancaarantes28@gmail.com>
2017-04-15 17:07:36 -0300

    Teste git no ubuntu

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 				<center><img src="imagens/direito.jpg" alt="Direito, escritório de advocacia" width="70%" class="img-quem-somos"></center>
 			</div>
 			<div class="col-md-8 col-xs-12 breve-texto">
-				<p>Jussara, tem a preocupação constante de sempre buscar a melhor e mais rápida solução para os problemas
+				<p>Jussara, tem a preocupação constante de sempre buscar buscar a melhor e mais rápida solução para os problemas
 				legais de nossos clientes seja pessoa física ou jurídica, e sempre com objetivo principal de buscar a 
 				excelência no atendimento aos clientes. Para tanto, desenvolvemos soluções criativas e eficazes, a uma
 				vasta gama de necessidades jurídicas e extrajurídicas, que vão das mais simples e rotineiras às mais complexas
